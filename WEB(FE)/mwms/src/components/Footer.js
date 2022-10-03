c577792fbae8e1ffaf4af1f73b9c6ae44cb565b7
yh2369 <62461435+yh2369@users.noreply.github.com>
2022-10-03 10:46:51 +0000

    modify MainPage

--- WEB(FE)/mwms/src/components/Footer.js
+++ WEB(FE)/mwms/src/components/Footer.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import logoImg from '../images/logo.png'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+      <footer class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="./" class="flex items-center mb-4 sm:mb-0">
+                <span class = "text-black-600 text-2xl font-bold">MWMS</span>
+            </a>
+            <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+                </li>
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="./" class="hover:underline">MWMS</a>. All Rights Reserved.
+        </span>
+     </footer>
+    </div>
   )
 }
 
--- WEB(FE)/mwms/src/pages/MainPage.js
+++ WEB(FE)/mwms/src/pages/MainPage.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import Header from "../components/Header"
+import Footer from "../components/Footer"
 
 function MainPage() {
   return (
     <div>
+      <body>
       <Header />
+      <div class="text-center mx-4 my-20 space-y-2" >
+        <h1 class="text-black-600 text-5xl font-bold">
+          국방물자관리체계<br />
+          Military Warehouse Management System
+        </h1>
+        
+        { /* TODO: Demo Video */}
+        <div class="grid h-screen place-items-center">
+          <iframe width="840" height="472" src="https://www.youtube.com/embed/7Dbu5M2utEA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <p>
+
+        </p>
+      </div>
+      <Footer />
+      </body>
     </div>
   )
 }
