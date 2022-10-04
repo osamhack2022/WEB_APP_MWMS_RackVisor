35bd74b2ee4db1abe7352b518d0f59f55c32bbc1
lhw414 <dlgusdn0414@snu.ac.kr>
2022-10-04 18:48:59 +0900

    temp2

--- WEB(BE)/src/src/models/box.js
+++ WEB(BE)/src/src/models/box.js
@@ -1,7 +1,6 @@
 const { sequelize } = require('sequelize');
 
 module.exports = class Box extends sequelize.Model {
-<<<<<<< HEAD
     static init(sequelize){
         super.init({
             id: {
@@ -32,35 +31,6 @@ module.exports = class Box extends sequelize.Model {
             collate: 'utf8_general_ci',
         });
     }
-=======
-  static init(sequelize) {
-    super.init(
-      {
-        id: {
-          type: sequelize.UUID,
-          defaltValue: sequelize.UUIDV4,
-          primaryKey: true,
-        },
-        floor: {
-          type: sequelize.INTEGER,
-        },
-        order: {
-          type: sequelize.INTEGER,
-        },
-      },
-      {
-        sequelize,
-        timestamps: true,
-        underScore: false,
-        modelName: 'Box',
-        tableName: 'boxes',
-        paranoid: true,
-        charset: 'utf8',
-        collate: 'utf8_general_ci',
-      }
-    );
-  }
->>>>>>> 048e33cfc77b1224e3b739136ed3f57b8592b431
 
   static associate(db) {
     db.Box.belongsTo(db.Rack);
