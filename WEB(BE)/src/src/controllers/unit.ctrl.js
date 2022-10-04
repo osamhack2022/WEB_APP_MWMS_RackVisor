eab31079d8acaa8299ec7b41b540bf1d3a17a8a1
lhw414 <dlgusdn0414@snu.ac.kr>
2022-10-04 18:59:23 +0900

    temp3

--- WEB(BE)/src/src/controllers/unit.ctrl.js
+++ WEB(BE)/src/src/controllers/unit.ctrl.js
@@ -1,22 +1,25 @@
-const { User, Unit } = require("../models");
+const { User, Unit } = require('../models');
 
 export const getUnits = async (req, res, next) => {
-    const units = await Unit.findall({
-        include: {
-                model: User,
-                as: 'UserUnit',
-            }
-    }).toJSON();
+  const units = await Unit.findall({
+    include: {
+      model: User,
+      as: 'UserUnit',
+    },
+  }).toJSON();
 
-    return res.send(units);
-}
+  return res.send(units);
+};
 
 export const addUnit = async (req, res, next) => {
-    const { name, comment } = req.body;
-    await Unit.create({
-        name: name,
-        comment: comment,
-    }, {
-        include: [User]
-    });
-}
+  const { name, comment } = req.body;
+  await Unit.create(
+    {
+      name: name,
+      comment: comment,
+    },
+    {
+      include: [User],
+    }
+  );
+};
