a9fd35247d0b9ee23a8725da06bd4bed2eaac4c6
yh2369 <62461435+yh2369@users.noreply.github.com>
2022-10-12 10:00:17 +0000

    물자관리 dropdown 수정

--- WEB(FE)/mwms/src/pages/authorPages/MaterialManage.js
+++ WEB(FE)/mwms/src/pages/authorPages/MaterialManage.js
@@ -5,18 +5,40 @@ import Footer from '../../components/Footer'
 import ExampleModal from '../../utils/modal/ExampleModal'
 import AuthorHeader from '../../components/AuthorHeader'
 import { AuthorContext } from '../../routes/Author'
-import {Link, useNavigate} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom'
 import { useAuth } from '../../routes/AuthContext'
 import Sidebar from '../../components/Sidebar'
-import SearchInput from '../../utils/search/SearchInput';
-import Example from '../../components/simple_striped';
+import SearchInput from '../../utils/search/SearchInput'
+import Example from '../../components/simple_striped'
 import WarehouseGridLayout from '../../utils/grid/WarehouseMaterial'
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import { getLSUnitList } from './UnitSelect'
 
 function MaterialManage() {
   const auth =useAuth();
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    if(auth.unitSelected === "") {
+      alert("부대를 선택해주세요");
+      navigate("/");
+    }
+  }, [])
+
+  // TODO: 서버로부터 unit(부대) 불러와야함...
+  let unitName = auth.unitSelected;
+  let lsUnitList=  getLSUnitList();
+  let lsUnit = lsUnitList.find( (e) => (e.name === unitName) );
+  let hl;
+  if(lsUnit === undefined)
+  {
+    hl = []
+  }
+  else
+  {
+    hl = lsUnit.houseList;
+  }
 
   return (
     <div>
@@ -33,10 +55,13 @@ function MaterialManage() {
             </div>
             <div class="gap-2">
               <span class="m-2 p-2 font-bold">위치 기반 물자 관리</span> <br /> 
-              <DropdownButton id="dropdown-basic-button" title="1종창고">
-                <Dropdown.Item href="#/action-1">1종창고</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">2종창고</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">3종창고</Dropdown.Item>
+              <DropdownButton id="dropdown-basic-button" title="창고를 선택하세요">
+              {
+                hl.map((h) => (
+                <Dropdown.Item href="#/action">{h.name}</Dropdown.Item> 
+              ))}
+                  
+                
               </DropdownButton>
               <WarehouseGridLayout /> {/* TODO: box 클릭했을때 1,2,3층별로 나눠진 modal 추가 하기 */}
             </div>
