1ccd61b688d047103c4de8ce6a15612417bea820
Poxios <poxios0310@gmail.com>
2022-10-12 15:34:16 +0900

    added jwt example for get:/users

--- WEB(BE)/src/index.ts
+++ WEB(BE)/src/index.ts
@@ -1,8 +1,14 @@
-import { Prisma, PrismaClient, User } from '@prisma/client';
-import { INSPECT_MAX_BYTES } from 'buffer';
-import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+import fastify, { FastifyInstance } from 'fastify';
+import jwt from './plugins/jwt';
+
 import userRoutes from './routes/user/user.route';
 import { userSchemas } from './routes/user/user.schema';
+
+// ! [Export] Decorated Fastify Instace Type (auto type inject is not supported on decorator)
+export type DecoratedFastifyInstance = FastifyInstance & {
+  authenticateWithJWT: any;
+};
 
 const prisma = new PrismaClient();
 const app = fastify();
@@ -11,6 +17,10 @@ for (const schema of [...userSchemas]) {
   app.addSchema(schema);
 }
 
+// ! [Register] JWT
+app.register(jwt);
+
+// ! [Register] Swagger
 app.register(require('@fastify/swagger'), {
   routePrefix: '/docs',
   swagger: {
@@ -27,27 +37,6 @@ app.register(require('@fastify/swagger'), {
     schemes: ['http'],
     consumes: ['application/json'],
     produces: ['application/json'],
-    // tags: [
-    //   { name: 'user', description: 'User related end-points' },
-    //   { name: 'code', description: 'Code related end-points' },
-    // ],
-    // definitions: {
-    //   User: {
-    //     type: 'object',
-    //     required: ['id', 'email'],
-    //     properties: {
-    //       id: { type: 'string', format: 'uuid' },
-    //       email: { type: 'string', format: 'email' },
-    //     },
-    //   },
-    // },
-    // securityDefinitions: {
-    //   apiKey: {
-    //     type: 'apiKey',
-    //     name: 'apiKey',
-    //     in: 'header',
-    //   },
-    // },
   },
   uiConfig: {
     docExpansion: 'full',
@@ -56,190 +45,15 @@ app.register(require('@fastify/swagger'), {
   exposeRoute: true,
 });
 
-//routing
+// ! [Register] Routing
 app.register(userRoutes, { prefix: 'api/users' });
 
+// ! [Server] Start Listening
 app.listen({ port: 3003 }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
-  console.log(`
-  🚀 Server ready at: http://localhost:3003
-  ⭐️ See sample requests: http://pris.ly/e/ts/rest-fastify#3-using-the-rest-api`);
+  console.log(`[DEV] Server Started at ${address}`);
+  console.log(`[DEV] 🚀 Server ready at: http://localhost:3003`);
 });
-
-// app.post<{
-//   Body: ISignupBody;
-// }>(`/signup`, async (req, res) => {
-//   const { name, email, posts } = req.body;
-
-//   const postData = posts?.map((post: Prisma.PostCreateInput) => {
-//     return { title: post?.title, content: post?.content };
-//   });
-
-//   const result = await prisma.user.create({
-//     data: {
-//       name,
-//       email,
-//       posts: {
-//         create: postData,
-//       },
-//     },
-//   });
-//   res.send(result);
-// });
-
-// app.post<{
-//   Body: ICreatePostBody;
-// }>(`/post`, async (req, res) => {
-//   const { title, content, authorEmail } = req.body;
-//   const result = await prisma.post.create({
-//     data: {
-//       title,
-//       content,
-//       author: { connect: { email: authorEmail } },
-//     },
-//   });
-//   res.send(result);
-// });
-
-// app.put<{
-//   Params: IPostByIdParam;
-// }>("/post/:id/views", async (req, res) => {
-//   const { id } = req.params;
-
-//   try {
-//     const post = await prisma.post.update({
-//       where: { id: Number(id) },
-//       data: {
-//         viewCount: {
-//           increment: 1,
-//         },
-//       },
-//     });
-
-//     res.send(post);
-//   } catch (error) {
-//     res.send({ error: `Post with ID ${id} does not exist in the database` });
-//   }
-// });
-
-// app.put<{
-//   Params: IPostByIdParam;
-// }>("/publish/:id", async (req, res) => {
-//   const { id } = req.params;
-
-//   try {
-//     const postData = await prisma.post.findUnique({
-//       where: { id: Number(id) },
-//       select: {
-//         published: true,
-//       },
-//     });
-
-//     const updatedPost = await prisma.post.update({
-//       where: { id: Number(id) || undefined },
-//       data: { published: !postData?.published },
-//     });
-//     res.send(updatedPost);
-//   } catch (error) {
-//     res.send({ error: `Post with ID ${id} does not exist in the database` });
-//   }
-// });
-
-// app.delete<{
-//   Params: IPostByIdParam;
-// }>(`/post/:id`, async (req, res) => {
-//   const { id } = req.params;
-//   const post = await prisma.post.delete({
-//     where: {
-//       id: Number(id),
-//     },
-//   });
-//   res.send(post);
-// });
-
-// app.get("/users", async (req, res) => {
-//   const users = await prisma.user.findMany();
-//   res.send(users);
-// });
-
-// app.get<{
-//   Params: IPostByIdParam;
-// }>("/user/:id/drafts", async (req, res) => {
-//   const { id } = req.params;
-
-//   const drafts = await prisma.user
-//     .findUnique({
-//       where: { id: Number(id) },
-//     })
-//     .posts({
-//       where: { published: false },
-//     });
-
-//   res.send(drafts);
-// });
-
-// app.get<{
-//   Params: IPostByIdParam;
-// }>(`/post/:id`, async (req, res) => {
-//   const { id } = req.params;
-
-//   const post = await prisma.post.findUnique({
-//     where: { id: Number(id) },
-//   });
-//   res.send(post);
-// });
-
-// app.get<{
-//   Querystring: IFeedQueryString;
-// }>("/feed", async (req, res) => {
-//   const { searchString, skip, take, orderBy } = req?.query;
-
-//   const or: Prisma.PostWhereInput = searchString
-//     ? {
-//         OR: [
-//           { title: { contains: searchString as string } },
-//           { content: { contains: searchString as string } },
-//         ],
-//       }
-//     : {};
-
-//   const posts = await prisma.post.findMany({
-//     where: {
-//       published: true,
-//       ...or,
-//     },
-//     include: { author: true },
-//     take: Number(take) || undefined,
-//     skip: Number(skip) || undefined,
-//     orderBy: {
-//       updatedAt: orderBy as Prisma.SortOrder,
-//     },
-//   });
-
-//   res.send(posts);
-// });
-// interface IFeedQueryString {
-//   searchString: string | null;
-//   skip: number | null;
-//   take: number | null;
-//   orderBy: Prisma.SortOrder | null;
-// }
-
-// interface IPostByIdParam {
-//   id: number;
-// }
-
-// interface ICreatePostBody {
-//   title: string;
-//   content: string | null;
-//   authorEmail: string;
-// }
-
-// interface ISignupBody {
-//   name: string | null;
-//   email: string;
-//   posts: Prisma.PostCreateInput[];
-// }
