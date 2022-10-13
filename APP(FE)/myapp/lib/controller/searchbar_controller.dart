c1eb019c6d33ca7ba01029dd0aca6b8fa2a93075
codingle2 <113528011+codingle2@users.noreply.github.com>
2022-10-13 15:03:26 +0000

    10_14

--- APP(FE)/myapp/lib/controller/searchbar_controller.dart
+++ APP(FE)/myapp/lib/controller/searchbar_controller.dart
@@ -1,23 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:myapp/model/unit_page_model.dart';
+import '../model/login_model.dart';
 import '../model/searchbar_model.dart';
+import '../services/web_service.dart';
 class SearchBarController extends GetxController {
+  Album album2;
 
-
-  static List<SearchbarModel> searchList = [
-
-  ];
-
-
-  List<SearchbarModel> displayList = List.from(searchList);
-
-  
-
-  void updateList(String value) {
-    
+  circularProgress() {
+    return const Center(child: CircularProgressIndicator());
   }
 
-  SearchBar() {
+  searchBar() {
     return TextField(
       style: const TextStyle(color: Colors.white),
     decoration: InputDecoration(
@@ -32,9 +26,59 @@ class SearchBarController extends GetxController {
         prefixIconColor: const Color.fromARGB(255, 255, 255, 255)
     )
   );
-
-
-
-
   }
+
+  searchResultList(Album album) {
+    return Card(
+      elevation: 5.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Container(
+          width: 200,
+          height: 100,
+          child: Center(
+            child: Text(album.title),
+          ),
+        ),
+    );
+  }
+
+    void upadateList() {
+    setState(( {
+      album.where((element) => element.title)
+    }))
+  }
+
+
+  searchResultListView(AsyncSnapshot<List<Album>> snapshot) {
+    return Expanded(
+      child: GestureDetector(
+          child: GridView.count(
+            crossAxisCount: 1,
+            childAspectRatio: 1.0,
+            mainAxisSpacing: 4.0,
+            crossAxisSpacing: 4.0,
+            children: snapshot.data!
+            .map(
+          (album) {
+            return GestureDetector(
+              child: GridTile(
+                child: searchResultList(album),
+                
+              ),
+              onTap: () {
+                //itemClick(snapshot);
+              },
+            );
+          },
+            ).toList(),
+        ),
+      ), 
+      );
+  }
+
+
+
 }
+
--- APP(FE)/myapp/lib/view/serach_page.dart
+++ APP(FE)/myapp/lib/view/serach_page.dart
@@ -12,48 +12,6 @@ class SearchPage extends StatelessWidget {
   SearchBarController searchBarController = Get.put(SearchBarController());
 
 
-gridview(AsyncSnapshot<List<Album>> snapshot) {
-    return Padding(
-      padding: const EdgeInsets.all(5.0),
-      child: GridView.count(
-        crossAxisCount: 2,
-        childAspectRatio: 1.0,
-        mainAxisSpacing: 4.0,
-        crossAxisSpacing: 4.0,
-        children: snapshot.data!
-        .map(
-          (album) {
-            return GestureDetector(
-              child: GridTile(
-                child: AlbumCell(album),
-              ),
-              onTap: () {
-                cellClick(album);
-              },
-            );
-          },
-        ).toList(),
-      ),
-    );
-  }
-
-  cellClick(Album album) {
-    Get.toNamed("/frontPage", arguments: album.title);
-
-    //페이지 데이터 삭제후 이동
-    //Get.off(() =>FrontPage(), arguments: album.title)
-  }
-
-  circularProgress() {
-    return const Center(child: CircularProgressIndicator());
-  }
-
-
-
-
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,26 +24,24 @@ gridview(AsyncSnapshot<List<Album>> snapshot) {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
 
-            InkWell(
-              child: searchBarController.SearchBar(),
-              
-              onTap: () {            
-                Expanded(
-                child: FutureBuilder<List<Album>>(
-                  future: WebService.getPhotos(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasError) {
-                      return Text('Error ${snapshot.error}');
-                    }
-                    if (snapshot.hasData) {
-                      return gridview(snapshot);
-                    }
-                    return circularProgress();
-                  }),
-              );
+       
+            searchBarController.searchBar(),
+             
+           
 
-              },
-            )
+            const SizedBox(height: 20.0,),
+
+            FutureBuilder<List<Album>>(
+                future: WebService.getPhotos(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Error ${snapshot.error}');
+                  }
+                  if (snapshot.hasData) {
+                    return Expanded(child: searchBarController.searchResultListView(snapshot));
+                  }
+                  return searchBarController.circularProgress();
+                }),     
           ],
         ),
       ),
