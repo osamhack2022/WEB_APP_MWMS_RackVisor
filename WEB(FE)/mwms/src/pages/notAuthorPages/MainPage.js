75738f8e98690969feef61cffcdc85e13eff3609
yh2369 <62461435+yh2369@users.noreply.github.com>
2022-10-16 06:34:23 +0000

    디자인은 어두운 테마로 갈 예정, Header.js 수정

--- WEB(FE)/mwms/src/pages/notAuthorPages/MainPage.js
+++ WEB(FE)/mwms/src/pages/notAuthorPages/MainPage.js
@@ -1,29 +1,47 @@
 import React from 'react'
 import Header from "../../components/Header"
 import Footer from "../../components/Footer"
-
+import styles from '../../style.js'
 
 function MainPage() {
   return (
-    <div>
-      <body>
-      <Header/>
-      <div class="text-center mx-4 my-20 space-y-2" >
-        <h1 class="text-black-600 text-5xl font-bold">
-          국방물자관리체계<br />
-          Military Warehouse Management System
-        </h1>
-        
-        { /* TODO: Demo Video */}
-        <div class="grid h-screen place-items-center">
-          <iframe width="840" height="472" src="https://www.youtube.com/embed/7Dbu5M2utEA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
-        <p>
+    <div class="bg-primary-900 w-full overflow-hidden">
 
-        </p>
+
+      <div class={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div class={`${styles.boxWidth}`}>
+          <Header/>
+        </div>
       </div>
-      <Footer />
-      </body>
+
+
+      <div class={`bg-primary  ${styles.flexStart}`}>
+        <div class={`${styles.boxwidth}`}>
+          <div class="text-center text-white mx-4 my-20 space-y-2" >
+          <h1 class="text-black-600 text-5xl font-bold">
+            국방물자관리체계<br />
+            Military Warehouse Management System
+          </h1>
+          
+          { /* TODO: Demo Video */}
+          <div class="grid h-screen place-items-center">
+            <iframe width="840" height="472" src="https://www.youtube.com/embed/7Dbu5M2utEA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div> 
+
+      <div class={`bg-primary $ ${styles.flexStart}`}>
+        <div class={`${styles.boxwidth}`}>
+        </div>
+      </div>
+
+      </div>
+
+      <div class={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div class={`${styles.boxWidth}`}>
+          <Footer />
+        </div>
+      </div>
     </div>
   )
 }
--- WEB(FE)/mwms/tailwind.config.js
+++ WEB(FE)/mwms/tailwind.config.js
@@ -6,8 +6,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a"}
+        primary: { "50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#00040f"},
+        secondary: "#00f6ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
       }
+    },
+        screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
     },
     fontFamily: {
       'body': [
@@ -25,7 +36,9 @@ module.exports = {
       'Apple Color Emoji', 
       'Segoe UI Emoji', 
       'Segoe UI Symbol', 
-      'Noto Color Emoji'
+      'Noto Color Emoji',
+      'Poppins',
+      'sans-serif',
     ],
       'sans': [
       'Inter', 
