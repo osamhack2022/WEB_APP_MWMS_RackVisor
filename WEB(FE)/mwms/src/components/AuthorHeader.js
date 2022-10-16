39d8b308a197e822bed5be8367c1f7e3f26373a5
TaehoJE <89475434+stpcoder@users.noreply.github.com>
2022-10-16 05:16:05 +0000

    꼬인 소스 풀기

--- WEB(FE)/mwms/src/components/AuthorHeader.js
+++ WEB(FE)/mwms/src/components/AuthorHeader.js
@@ -35,7 +35,7 @@ function AuthorHeader() {
               <AlarmDropDown/>
               <span>{position}</span>
               <UserDropDown milClass={classes} name={name}/>
-              <Link to="/cost">환경설정</Link>
+              {/* <Link to="/cost">환경설정</Link> */}
             </ul>
           </div>
         </div>
--- WEB(FE)/mwms/src/pages/authorPages/BarcodeManage.js
+++ WEB(FE)/mwms/src/pages/authorPages/BarcodeManage.js
@@ -112,6 +112,15 @@ function BarcodeManage() {
   const valList = ['이름', '종류', '세부분류', '수량', '상태', '기한']
   const data = [{'이름' : '휴지', '종류' : '2종', '세부분류' : '기타물자류', '수량':1000, '상태':'좋음', '기한':'2022/10/27'}]
   const [ item, setItem ] = useState("");
+  const [size, setSize] = useState(0);
+  const [count, setCount] = useState(0);
+  const [convert, setConvert] = useState("");
+  const [printArr, setPrintArr] = useState([]);
+  const [cname, setCName] = useState("");
+  const [showName, setShowName] = useState(false);
+  const [showDue, setShowDue] = useState(false);
+  const [showManager, setShowManager] = useState(false);
+  const [showLocation, setShowLocation] = useState(false);
 
   const generateQrCode = async (textInput) => {
     try {
@@ -122,6 +131,34 @@ function BarcodeManage() {
     }
   }
 
+  const handleSize = (e) => {
+    setSize(e.target.value);
+    setConvert((e.target.value * 10).toString() + "mm");
+
+    let gridCnt = parseInt(20 / (e.target.value));
+    if (gridCnt > 5) {
+      setCName("grid grid-cols-5");
+    } else if (gridCnt == 3) {
+      if ((20 / (e.target.value)) > 3.5) {
+        setCName("grid grid-cols-4");
+      } else {
+        setCName("grid grid-cols-2");
+      }
+    } else if (gridCnt > 0) {
+      setCName("grid grid-cols-" + gridCnt.toString());
+    }
+  }
+
+  const handleCount = (e) => {
+    setCount(e.target.value);
+    let i;
+    let inputArr = [];
+    for (i = 0; i < e.target.value; i++) {
+      inputArr.push("1");
+    }
+    setPrintArr(inputArr);
+  }
+  
   useEffect(() => {
     if (auth.unitSelected === "") {
       alert("부대를 다시 선택해주세요");
@@ -150,10 +187,53 @@ function BarcodeManage() {
             {/* <input class="border" onChange={(e) => setText(e.target.value)}/>
             <button class="border" onClick={() => generateQrCode()}>QR코드 생성</button>
               <br/> */}
+              <div className="flex">
+                <div>사이즈 입력 {'(cm 단위)'} : </div>
+                <input type="number" value={size} onChange={handleSize} className="border"/>
+              </div>
+              <div className="flex">
+                <div>개수 입력 : </div>
+                <input type="number" value={count} onChange={handleCount} className="border"/>
+              </div>
+              <div className="flex">
+                <input type="checkbox" value={showName} onChange={() => setShowName(!showName)} className="border"/>
+                <div>이름 출력</div>
+              </div>
+              <div className="flex">
+                <input type="checkbox" value={showDue} onChange={() => setShowDue(!showDue)} className="border"/>
+                <div>유통기한 출력</div>
+              </div>
+              <div className="flex">
+                <input type="checkbox" value={showManager} onChange={() => setShowManager(!showManager)} className="border"/>
+                <div>담당자 출력</div>
+              </div>
+              <div className="flex">
+                <input type="checkbox" value={showLocation} onChange={() => setShowLocation(!showLocation)} className="border"/>
+                <div>위치 출력</div>
+              </div>
               {imageUrl ? (
+              <>
               <a href={imageUrl} download>
-                  <div  ref={printR} class="display-none"><img src={imageUrl} alt="img"/>eoeo</div>
-              </a>) : null}
+                <img src={imageUrl} alt="img" style={{width:"100px", height:"100px"}} className="border"/>
+                {showName && <div className="flex">휴지</div>}
+                {showDue && <div className="flex">2022-10-27</div>}
+                {showManager && <div className="flex">홍길동</div>}
+                {showLocation && <div className="flex">2종창고 - A 캐비넷</div>}
+              </a>
+              <div className="hidden">  
+                <div ref={printR} className= {cname}>
+                  {printArr.map(() => (
+                    <div className="">
+                      <img src={imageUrl} alt="img" style={{width: convert, height: convert}} className="border flex"/>
+                      {showName && <div className="flex">휴지</div>}
+                      {showDue && <div className="flex">2022-10-27</div>}
+                      {showManager && <div className="flex">홍길동</div>}
+                      {showLocation && <div className="flex">2종창고 - A 캐비넷</div>}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </>) : null}
             <ReactToPrint
               trigger={() => <button>프린트하기</button>}
               content={() => printR.current}
