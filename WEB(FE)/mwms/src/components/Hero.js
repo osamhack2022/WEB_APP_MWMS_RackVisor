be2ebbae054cec6d62653fc5eca6f0f24ed091d7
yh2369 <62461435+yh2369@users.noreply.github.com>
2022-10-16 09:36:05 +0000

    Hero.js 추가

---
+++ WEB(FE)/mwms/src/components/Hero.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import styles from '../style';
+import warehouseImg from '../images/warehouse.png'
+
+function Hero() 
+{
+    return (
+        <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+        <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+          <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+            
+            <p className={`${styles.paragraph} ml-2`}>
+              MWMS
+            </p>
+          </div>
+  
+          <div className="flex flex-row justify-between items-center w-full">
+            <h1 className="flex-1 font-poppins font-semibold ss:text-[60px] text-[48px] text-white ss:leading-[100.8px] leading-[75px]">
+                국방물자관리체계 <br className="sm:block hidden" />{" "}
+              <span className="text-gradient">Military Warehouse</span>{" "}
+            </h1>
+          </div>
+  
+          <h1 className="font-poppins font-semibold ss:text-[56px] text-[48px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+            Management System
+          </h1>
+          <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          국방 물자 관리 체계를 통해서 군 부대의 여러 물품과 자재들을 효과적으로 보관 및 관리를 할 수 있습니다.
+          </p>
+        </div>
+  
+        <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+          <img src={warehouseImg} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+  
+          {/* gradient start */}
+          <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+          <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+          <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+          {/* gradient end */}
+        </div>
+  
+      </section>
+    )
+}
+
+export default Hero
--- WEB(FE)/mwms/src/pages/notAuthorPages/MainPage.js
+++ WEB(FE)/mwms/src/pages/notAuthorPages/MainPage.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import Header from "../../components/Header"
-import Footer from "../../components/Footer"
-import styles from '../../style.js'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import styles from '../../style'
+import Hero from '../../components/Hero'
 
 function MainPage() {
   return (
@@ -15,33 +16,18 @@ function MainPage() {
       </div>
 
 
-      <div class={`bg-primary  ${styles.flexStart}`}>
-        <div class={`${styles.boxwidth}`}>
-          <div class="text-center text-white mx-4 my-20 space-y-2" >
-          <h1 class="text-black-600 text-5xl font-bold">
-            국방물자관리체계<br />
-            Military Warehouse Management System
-          </h1>
-          
-          { /* TODO: Demo Video */}
-          <div class="grid h-screen place-items-center">
-            <iframe width="840" height="472" src="https://www.youtube.com/embed/7Dbu5M2utEA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
+      <div class={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div class={`${styles.boxWidth}`}>
+          <Hero/>
         </div>
-      </div> 
-
-      <div class={`bg-primary $ ${styles.flexStart}`}>
-        <div class={`${styles.boxwidth}`}>
-        </div>
-      </div>
-
       </div>
 
       <div class={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div class={`${styles.boxWidth}`}>
-          <Footer />
+          
         </div>
       </div>
+
     </div>
   )
 }
