32802ca346b6d32b5ece8075001cf07eb2a26cff
yh2369 <62461435+yh2369@users.noreply.github.com>
2022-10-16 13:23:53 +0000

    GetStarted 추가

--- WEB(FE)/mwms/src/components/Hero.js
+++ WEB(FE)/mwms/src/components/Hero.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '../style';
 import warehouseImg from '../images/warehouse.png'
+import GetStarted from "./GetStarted"
 
 function Hero() 
 {
@@ -15,10 +16,13 @@ function Hero()
           </div>
   
           <div class="flex flex-row justify-between items-center w-full">
-            <h1 class="flex-1 font-poppins font-semibold ss:text-[60px] text-[48px] text-white ss:leading-[100.8px] leading-[75px]">
+            <h1 class="flex-1 font-poppins font-semibold ss:text-[56px] text-[48px] text-white ss:leading-[100.8px] leading-[75px]">
                 국방물자관리체계 <br class="sm:block hidden" />{" "}
               <span class="text-gradient">Military Warehouse</span>{" "}
             </h1>
+            <div className="ss:flex hidden md:mr-2 mr-0">
+              <GetStarted />
+            </div>
           </div>
   
           <h1 class="font-poppins font-semibold ss:text-[56px] text-[48px] text-white ss:leading-[100.8px] leading-[75px] w-full">
@@ -37,6 +41,10 @@ function Hero()
           <div class="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
           <div class="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
           {/* gradient end */}
+        </div>
+
+        <div className={`ss:hidden ${styles.flexCenter}`}>
+         <GetStarted />
         </div>
   
       </section>
