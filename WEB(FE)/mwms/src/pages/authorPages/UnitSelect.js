266a6d2901d0a18702e27ed65120281a7031e194
Poxios <poxios0310@gmail.com>
2022-10-19 15:12:58 +0900

    unit fetch list / add done

--- WEB(FE)/mwms/src/pages/authorPages/UnitSelect.js
+++ WEB(FE)/mwms/src/pages/authorPages/UnitSelect.js
@@ -1,33 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import Footer from '../../components/Footer';
-import { useAuth } from '../../routes/AuthContext';
-import { useNavigate } from 'react-router-dom';
-import AuthorHeader from '../../components/AuthorHeader';
-import Title from '../../utils/with_description';
-import styles from '../../style.js';
-import Button from '../../components/Button';
-import { axiosPost } from '../../api';
+import React, { useCallback, useEffect, useState } from "react";
+import Footer from "../../components/Footer";
+import { useAuth } from "../../routes/AuthContext";
+import { useNavigate } from "react-router-dom";
+import AuthorHeader from "../../components/AuthorHeader";
+import Title from "../../utils/with_description";
+import styles from "../../style.js";
+import Button from "../../components/Button";
+import { axiosGet, axiosPost } from "../../api";
 
-// lsUnitList 예시 [{name: "123부대"}, {name: "456부대"}, ... ]
-export function getLSUnitList()
-{
-  let lsUnitList = localStorage.getItem("unitList");
-  if(lsUnitList === null)
-  {
-    lsUnitList = [];
-  }
-  else
-  {
-    lsUnitList = JSON.parse(lsUnitList);
-  }
-  return lsUnitList;
-}
-
-let lsUnitList = getLSUnitList();
+export const getLSUnitList = () => [];
 
 function UnitSelect() {
   const navigate = useNavigate();
-  const [unitList, setUnitList] = useState(lsUnitList);
+  const [unitList, setUnitList] = useState([]);
   //11번째 줄에서 서버에서 unit의 리스트를 받아오면 된다
   let auth = useAuth();
 
@@ -36,38 +21,39 @@ function UnitSelect() {
     localStorage.setItem("부대", e.target.id);
     e.stopPropagation();
     navigate("/main");
-  }
+  };
 
   const addUnit = async () => {
     const newName = prompt("부대명을 입력해주세요"); //부대명이 서로 달라야 함
     if (newName === null) {
       return;
     }
-    let lsUnitList = getLSUnitList();
-    lsUnitList.push({
-      name: newName,
-      houseList: [],
-    });
 
     // ? Add Unit
-    await axiosPost("/units", {
+    const itemToAdd = {
       name: newName,
       comment: newName,
-    });
-    localStorage.setItem("unitList", JSON.stringify(lsUnitList));
-    setUnitList(lsUnitList);
-  }
+    };
+    await axiosPost("/units", itemToAdd);
+    setUnitList((prev) => [...prev, itemToAdd]);
+  };
+
+  const fetchUnitList = useCallback(async () => {
+    try {
+      const data = await axiosGet("/units/all-units");
+      setUnitList(data);
+    } catch (error) {
+      alert("Error on fetching unit");
+    }
+  }, []);
 
   useEffect(() => {
     auth.unitSelect("");
-    let lsUnitList = getLSUnitList();
-    setUnitList(lsUnitList);
-    localStorage.setItem("부대", "");
+    fetchUnitList();
   }, []);
 
   return (
     <div class="bg-primary-900 w-full">
-
       <div class={`bg-primary-900 ${styles.flexCenter}`}>
         <div class={`${styles.boxWidth}`}>
           <AuthorHeader />
@@ -78,9 +64,18 @@ function UnitSelect() {
       <div class={`h-screen mx-48 px-32 `}>
         <div class="grid grid-cols-4 gap-4 px-4 py-3">
           {unitList.map((un) => (
-            <Button id={un.name} text={un.name} handleClick={onSelectUnit} class="hover:border-green-500 py-4 px-6 font-poppins font-bold text-[24px] text-primary-900 bg-blue-gradient border-2 border-white rounded-[10px]" />
+            <Button
+              id={un.name}
+              text={un.name}
+              handleClick={onSelectUnit}
+              class="hover:border-green-500 py-4 px-6 font-poppins font-bold text-[24px] text-primary-900 bg-blue-gradient border-2 border-white rounded-[10px]"
+            />
           ))}
-          <Button text={"+ 부대 추가"} handleClick={addUnit} class="hover:border-green-500 py-4 px-6 font-poppins font-bold text-[24px] text-primary-900 bg-blue-gradient border-2 border-white rounded-[10px]"/>
+          <Button
+            text={"+ 부대 추가"}
+            handleClick={addUnit}
+            class="hover:border-green-500 py-4 px-6 font-poppins font-bold text-[24px] text-primary-900 bg-blue-gradient border-2 border-white rounded-[10px]"
+          />
         </div>
       </div>
 
@@ -89,9 +84,8 @@ function UnitSelect() {
           <Footer />
         </div>
       </div>
-    
     </div>
-  )
+  );
 }
 
-export default UnitSelect
+export default UnitSelect;
