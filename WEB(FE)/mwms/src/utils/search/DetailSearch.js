8c795d7851db4edc65eb3e6f358bccd4dd31fe71
Poxios <poxios0310@gmail.com>
2022-10-23 14:52:30 +0900

    detail search done

--- WEB(FE)/mwms/src/utils/search/DetailSearch.js
+++ WEB(FE)/mwms/src/utils/search/DetailSearch.js
@@ -1,13 +1,14 @@
-import React, {useState} from 'react';
-import {detailType} from './typeList';
-import DatePicker, { registerLocale } from 'react-datepicker';
+import React, { useEffect, useState } from "react";
+import { detailType } from "./typeList";
+import DatePicker, { registerLocale } from "react-datepicker";
 import "./datapicker.css";
 import { ko } from "date-fns/esm/locale";
 import "./react-datepicker.css";
 
-export default function DetailSearch() {
-  const [Content, setContent] = useState('없음');
-  const [type, setType] = useState('없음');
+export default function DetailSearch(props) {
+  const { setAdvancedSearchReqBody } = props;
+  const [Content, setContent] = useState("없음");
+  const [type, setType] = useState("없음");
   const [minCnt, setMinCnt] = useState(0);
   const [maxCnt, setMaxCnt] = useState(0);
   const [people, setPeople] = useState("");
@@ -15,36 +16,59 @@ export default function DetailSearch() {
   const [endDate, setEndDate] = useState(new Date());
 
   const [chkbox, setChkbox] = useState({
-    'property' : false,
-    'duration' : false,
-    'manager' : false,
-    'quantity' : false,
-  }); 
+    property: false,
+    duration: false,
+    manager: false,
+    quantity: false,
+  });
 
   const [ch1, set1] = useState(false);
   const [ch2, set2] = useState(false);
   const [ch3, set3] = useState(false);
   const [ch4, set4] = useState(false);
 
+  useEffect(() => {
+    const toStringAndPadLeftWithZero = (e, maxLength) => e.toString().padStart(maxLength, "0");
+
+    const getYYYYMMDDFromDate = (date) => {
+      return (
+        toStringAndPadLeftWithZero(date.getFullYear(), 4) +
+        "/" +
+        toStringAndPadLeftWithZero(date.getMonth() + 1, 2) +
+        "/" +
+        toStringAndPadLeftWithZero(date.getDate(), 2)
+      );
+    };
+
+    setAdvancedSearchReqBody({
+      type: "TYPE_" + (parseInt(Content) || "NULL"),
+      minExpDate: startDate && getYYYYMMDDFromDate(startDate),
+      maxExpDate: endDate && getYYYYMMDDFromDate(endDate),
+      manager: people,
+      minAmount: minCnt,
+      maxAmount: maxCnt,
+    });
+  }, [startDate, endDate, people, minCnt, maxCnt, setAdvancedSearchReqBody, type, Content]);
+
   const chgPeople = (e) => {
     setPeople(e.currentTarget.value);
-  }
+  };
 
   const chgMinCnt = (e) => {
     setMinCnt(e.currentTarget.value);
-  }
+  };
 
   const chgMaxCnt = (e) => {
     setMaxCnt(e.currentTarget.value);
-  }
+  };
 
   const onChangeHanlder = (e) => {
-  	setContent(e.currentTarget.value);
-  }
+    setContent(e.currentTarget.value);
+  };
   const onChangeType = (e) => {
     setType(e.currentTarget.value);
-  }
-  
+  };
+
   const onChkBox = (e) => {
     let copy = chkbox;
     copy[e.currentTarget.id] = !copy[e.currentTarget.id];
@@ -60,12 +84,18 @@ export default function DetailSearch() {
     }
 
     setChkbox(copy);
-  }
+  };
 
   return (
     <div class="p-2 text-[12px] border border-gray-300 rounded rounded-lg m-2 max-w-[600px]">
       <div class="flex">
-        <input class="mx-2 bg-[#706F6F] accent-pink-500" id = "property" type="checkbox" checked={ch1} onChange={onChkBox}/>
+        <input
+          class="mx-2 bg-[#706F6F] accent-pink-500"
+          id="property"
+          type="checkbox"
+          checked={ch1}
+          onChange={onChkBox}
+        />
         <div class="text-white font-bold mx-2">속성 : </div>
         <div>
           <select class="bg-[#706F6F] text-white rounded" onChange={onChangeHanlder} value={Content}>
@@ -83,9 +113,15 @@ export default function DetailSearch() {
       </div>
 
       <div class="flex">
-        <input class="mx-2 my-2 bg-[#706F6F] accent-pink-500" id = "duration" type="checkbox" checked={ch2} onChange={onChkBox}/>
+        <input
+          class="mx-2 my-2 bg-[#706F6F] accent-pink-500"
+          id="duration"
+          type="checkbox"
+          checked={ch2}
+          onChange={onChkBox}
+        />
         <div class="text-white font-bold mx-2">기한 : </div>
-        <DatePicker 
+        <DatePicker
           locale={ko}
           dateFormat="yyyy년 MM월 dd일"
           selected={startDate}
@@ -95,7 +131,7 @@ export default function DetailSearch() {
           endDate={endDate}
         />
         <div class="text-white font-semibold mx-2"> 부터 ~ </div>
-        <DatePicker 
+        <DatePicker
           locale={ko}
           dateFormat="yyyy년 MM월 dd일"
           selected={endDate}
@@ -109,17 +145,44 @@ export default function DetailSearch() {
       </div>
 
       <div class="flex">
-        <input class="mx-2 my-2 bg-[#706F6F] accent-pink-500" id = "manager" type="checkbox" checked={ch3} onChange={onChkBox}/>
+        <input
+          class="mx-2 my-2 bg-[#706F6F] accent-pink-500"
+          id="manager"
+          type="checkbox"
+          checked={ch3}
+          onChange={onChkBox}
+        />
         <div class="text-white font-bold mx-2">담당자 : </div>
-        <input type="string" class="bg-[#706F6F] h-[22px] p-2 text-white rounded border" value={people} onChange={chgPeople}/>
+        <input
+          type="string"
+          class="bg-[#706F6F] h-[22px] p-2 text-white rounded border"
+          value={people}
+          onChange={chgPeople}
+        />
       </div>
 
       <div class="flex">
-        <input class="mx-2 my-2 border bg-[#706F6F] accent-pink-500" id = "quantity" type="checkbox" checked={ch4} onChange={onChkBox}/>
-        <div class="text-white font-bold mx-2">{'수량 : '}</div>
-        <input class="mx-2 my-2 border bg-[#706F6F] text-white rounded h-[22px] p-2" type="number" value={minCnt} onChange={chgMinCnt}/>
+        <input
+          class="mx-2 my-2 border bg-[#706F6F] accent-pink-500"
+          id="quantity"
+          type="checkbox"
+          checked={ch4}
+          onChange={onChkBox}
+        />
+        <div class="text-white font-bold mx-2">{"수량 : "}</div>
+        <input
+          class="mx-2 my-2 border bg-[#706F6F] text-white rounded h-[22px] p-2"
+          type="number"
+          value={minCnt}
+          onChange={chgMinCnt}
+        />
         <div class="text-white font-bold mx-2">이상 ~ </div>
-        <input class="mx-2 my-2 border bg-[#706F6F] text-white rounded h-[22px] p-2" type="number" value={maxCnt} onChange={chgMaxCnt}/>
+        <input
+          class="mx-2 my-2 border bg-[#706F6F] text-white rounded h-[22px] p-2"
+          type="number"
+          value={maxCnt}
+          onChange={chgMaxCnt}
+        />
         <div class="text-white font-bold mx-2">이하</div>
       </div>
     </div>
--- WEB(FE)/mwms/src/utils/search/SearchInput.js
+++ WEB(FE)/mwms/src/utils/search/SearchInput.js
@@ -1,40 +1,77 @@
-import React, { useState } from 'react'
-import DetailSearch from './DetailSearch';
-import { SearchIcon } from '@heroicons/react/solid'
+import React, { useState } from "react";
+import DetailSearch from "./DetailSearch";
+import { SearchIcon } from "@heroicons/react/solid";
+import { axiosPost } from "../../api";
 
-
-function SearchInput({search, setSearch}) {
+function SearchInput({ search, setSearch }) {
   const [name, setName] = useState();
   const [detail, setDetail] = useState(false);
+  const [advancedSearchReqBody, setAdvancedSearchReqBody] = useState({});
 
   const onNameHandle = (e) => {
     setName(e.target.value);
-  }
+  };
 
+  const onSearch = async () => {
+    const reqBody = {
+      ...advancedSearchReqBody,
+      name,
+    };
+    console.log(reqBody);
+    const result = await axiosPost("/stocks/advanced-search", reqBody);
+    console.log(result);
+  };
 
   return (
     <form>
-      <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
+      <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only">
+        Search
+      </label>
       <div class="flex justify-center">
         <div class="relative">
-        <div class="absolute inset-y-10 left-0 items-center pl-3 pointer-events-none">
-            <svg aria-hidden="true" class="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-        </div>
-        <input class="block p-4 pl-10 w-[36rem] text-sm text-white rounded-2xl bg-[#202020] focus:border mt-6" placeholder="군수품명" required="" value={name} onChange={onNameHandle}/>
-        <SearchIcon className="h-9 w-9 px-1 absolute right-2 bottom-2.5 rounded-full text-white hover:bg-[#7A5EA6]"/>
+          <div class="absolute inset-y-10 left-0 items-center pl-3 pointer-events-none">
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-gray-500"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+            </svg>
+          </div>
+          <input
+            class="block p-4 pl-10 w-[36rem] text-sm text-white rounded-2xl bg-[#202020] focus:border mt-6"
+            placeholder="군수품명"
+            required=""
+            value={name}
+            onChange={onNameHandle}
+          />
+          <SearchIcon
+            className="h-9 w-9 px-1 absolute right-2 bottom-2.5 rounded-full text-white hover:bg-[#7A5EA6]"
+            onClick={onSearch}
+          />
         </div>
       </div>
       {!detail && (
-        <button class="ml-4 mt-2 text-[#5AB0AD] font-poppins font-semibold mb-4" onClick={() => setDetail(true)}>상세검색</button>
+        <button class="ml-4 mt-2 text-[#5AB0AD] font-poppins font-semibold mb-4" onClick={() => setDetail(true)}>
+          상세검색
+        </button>
       )}
       {detail && (
-        <button class="ml-4 mt-2 text-white font-poppins rounded-lg p-1 font-semibold bg-[#5AB0AD] mb-4" onClick={() => setDetail(false)}>상세검색</button>
+        <button
+          class="ml-4 mt-2 text-white font-poppins rounded-lg p-1 font-semibold bg-[#5AB0AD] mb-4"
+          onClick={() => setDetail(false)}>
+          상세검색
+        </button>
       )}
-      {detail && (
-        <DetailSearch/>
-      )}
+      {detail && <DetailSearch setAdvancedSearchReqBody={setAdvancedSearchReqBody} />}
     </form>
-  )
+  );
 }
 
-export default SearchInput
+export default SearchInput;
